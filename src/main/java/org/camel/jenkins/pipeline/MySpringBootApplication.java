1d39874eb8e47bcff45099bb501abc89c1c327e7
DouglasGo8 <abraham.dias.douglas@gmail.com>
2021-05-24 19:14:16 -0300

    Linux Service

--- src/main/java/org/camel/jenkins/pipeline/MySpringBootApplication.java
+++ src/main/java/org/camel/jenkins/pipeline/MySpringBootApplication.java
@@ -2,6 +2,8 @@ package org.camel.jenkins.pipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MySpringBootApplication {
@@ -11,6 +13,16 @@ public class MySpringBootApplication {
      */
     public static void main(String[] args) {
         SpringApplication.run(MySpringBootApplication.class, args);
+    }
+
+
+    @RestController
+    public static class MyApi {
+
+        @GetMapping
+        public String sayHi() {
+            return "Hi";
+        }
     }
 
 }
